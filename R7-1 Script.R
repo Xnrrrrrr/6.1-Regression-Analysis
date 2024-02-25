33e83dab164945999ab6b184cfa1dd014cdf9bcc
xnrrrrrr <xnrrrrrr@gmail.com>
2024-02-25 11:46:01 -0500

    updated regression model and independence of observation

--- R7-1 Script.R
+++ R7-1 Script.R
@@ -1,3 +1,19 @@
 #Read the data into a dataframe
 
 lab6.data <- read.csv("lab 6 sra 365 wc-1.csv", header = TRUE)
+
+#Test your regression model for independence of observations.
+
+#Install required package
+install.packages("car")
+
+# Load the required package
+library(car)
+
+# Run a regression model
+lab6.reg <- lm(dys_detect ~ per_sensitive, data = lab6.data)
+
+#Run the Durbin-Watson test on your regression model.
+dwt(lab6.reg)
+
+
